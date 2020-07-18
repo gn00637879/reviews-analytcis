dae9621f198e5eb32dc2c9cbbc102dda60fe34d3
Rex Chang <gn00637879@gmail.com>
2020-07-18 18:33:17 +0800

    add word cound function

--- read.py
+++ read.py
@@ -5,11 +5,68 @@ with open ('reviews.txt', 'r') as f:
 		data.append(line)
 		count += 1
 		if count % 10000 == 0:
-			print(len(data))
+			print(len(data)) 
 print('檔案讀取完畢,留言共計有', len(data), '筆資料')
 
-sum_len = 0
-for d in data :
-	sum_len += len(d)
-print('留言總字數', sum_len)
-print('每筆平均留言字數', int(sum_len/len(data)),'個字' )
+wc = {} #word_count 
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word,wc[word])
+#print(len(wc))
+#print(wc['Rex'])
+
+while True:
+	word = input('你想查什麼字: ')
+	if word == 'q':
+		print('感謝使用本查詢')
+		break
+	if word in wc:
+		print(word,'出現過的次數為', wc[word], '次')
+	else:
+		print('沒有出現過')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# sum_len = 0
+# for d in data :
+# 	sum_len += len(d)
+# print('留言總字數', sum_len)
+# print('每筆平均留言字數', int(sum_len/len(data)),'個字' )
+
+
+# nice = [d for d in data if'nice' in d]
+# print('提到nice的留言共有', len(nice),'筆資料')
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('提到good的留言共有', len(good),'筆資料')
+
+# bad = ['bad' in d for d in data]
+# print(len(bad))
+
+
+
+
+
